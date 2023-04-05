6332b4cb34b7e7515b3c768abe407c0957c5de07
Paul Ruvolo <paullundyruvolo@gmail.com>
2023-04-05 16:07:13 -0400

    Initial Commit

---
+++ STEP Navigation/STEP_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEP_NavigationApp.swift
+//  STEP Navigation
+//
+//  Created by Paul Ruvolo on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct STEP_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
