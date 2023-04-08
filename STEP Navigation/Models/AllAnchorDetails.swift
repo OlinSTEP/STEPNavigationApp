6efa81e0efd04a4273d04ad191de7c9ec11b397a
Evelyn Kessler <molly@healthea.org>
2023-04-08 14:09:47 -0400

    removed the location address from the anchor details models

--- STEP Navigation/Models/AllAnchorDetails.swift
+++ STEP Navigation/Models/AllAnchorDetails.swift
@@ -9,9 +9,9 @@ import Foundation
 
 extension AnchorDetails {
     static let testAnchors: [AnchorDetails] = [
-        AnchorDetails(name: "Test Anchor 1 - Bus Stop", notes: "Notes about this location", locationAddress: "1000 Test Way", locationCoordinates: "coordinates here", anchorType: .busStop),
-        AnchorDetails(name: "Test Anchor 2 - Bus Stop", notes: "Notes about this location", locationAddress: "1002 Test Way", locationCoordinates: "coordinates here", anchorType: .busStop),
-        AnchorDetails(name: "Test Anchor 3 - Door", notes: "Notes about this location", locationAddress: "1003 Test Way", locationCoordinates: "coordinates here", anchorType: .externalDoor),
-        AnchorDetails(name: "Test Anchor 4 - Bathroom", notes: "Notes about this location", locationAddress: "1004 Test Way", locationCoordinates: "coordinates here", anchorType: .bathroom),
+        AnchorDetails(name: "Test Anchor 1 - Bus Stop", notes: "Notes about this location", locationCoordinates: "coordinates here", anchorType: .busStop),
+        AnchorDetails(name: "Test Anchor 2 - Bus Stop", notes: "Notes about this location", locationCoordinates: "coordinates here", anchorType: .busStop),
+        AnchorDetails(name: "Test Anchor 3 - Door", notes: "Notes about this location", locationCoordinates: "coordinates here", anchorType: .externalDoor),
+        AnchorDetails(name: "Test Anchor 4 - Bathroom", notes: "Notes about this location", locationCoordinates: "coordinates here", anchorType: .bathroom),
     ]
 }
--- STEP Navigation/Models/AnchorDetails.swift
+++ STEP Navigation/Models/AnchorDetails.swift
@@ -12,20 +12,18 @@ struct AnchorDetails: Identifiable {
     
     var name: String
     var notes: String
-    var locationAddress: String
     var locationCoordinates: String //what format are we storing the location in??
     var anchorType: AnchorType
     
-    init(name: String, notes: String, locationAddress: String, locationCoordinates: String, anchorType: AnchorType) {
+    init(name: String, notes: String, locationCoordinates: String, anchorType: AnchorType) {
         self.name = name
         self.notes = notes
-        self.locationAddress = locationAddress
         self.locationCoordinates = locationCoordinates
         self.anchorType = anchorType
     }
     
     init() {
-        self.init(name: "", notes: "", locationAddress: "", locationCoordinates: "", anchorType: .busStop)
+        self.init(name: "", notes: "", locationCoordinates: "", anchorType: .busStop)
     }
     
     enum AnchorType: String, CaseIterable {
