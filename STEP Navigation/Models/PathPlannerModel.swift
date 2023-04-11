7e09336f946cbb00333e81c7674f9738f86e2658
Sam Coleman <scoleman@olin.edu>
2023-04-11 15:27:18 -0400

    Edit PathPlannerModel and Tests

--- STEP Navigation/Models/PathPlannerModel.swift
+++ STEP Navigation/Models/PathPlannerModel.swift
@@ -5,59 +5,50 @@
 //  Created by Paul Ruvolo on 4/5/23.
 //
 
-// TODO which of these imports is actually needed?
 import Foundation
-import ARKit
-import ARCore
-import ARCoreCloudAnchors
 import ARCoreGeospatial
 
-class PathFinderModel {
-    /// The current ARFrame.
-    private var frame: ARFrame?
-    
-    /// The next node in the path (where we want to go).
-    private var nextNode: SCNNode?
-    
-    /// The straight line distance to the next node in meters.
-    private var straightDistance: Float?
-    
-    /// The angle difference between the current direction and the direction to the next node in degrees.
-    private var angleDifference: Float?
-    
-    /// Initializes a new instance of the PathFinderModel with the given ARFrame and SCNNode.
-    ///
-    /// - parameter frame: The current ARFrame.
-    /// - parameter nextNode: The next node in the path.
-    init(frame: ARFrame, nextNode: SCNNode) {
-        self.frame = frame
-        self.nextNode = nextNode
-    }
+class PathPlannerModel {
+//    /// The current ARFrame.
+//    private var frame: ARFrame?
+//
+//    /// The next node in the path (where we want to go).
+//    private var nextNode: SCNNode?
+//
+//    /// The straight line distance to the next node in meters.
+//    private var straightDistance: Float?
+//
+//    /// The angle difference between the current direction and the direction to the next node in degrees.
+//    private var angleDifference: Float?
+//
+//    /// Initializes a new instance of the PathFinderModel with the given ARFrame and SCNNode.
+//    ///
+//    /// - parameter frame: The current ARFrame.
+//    /// - parameter nextNode: The next node in the path.
+//    init(frame: ARFrame, nextNode: SCNNode) {
+//        self.frame = frame
+//        self.nextNode = nextNode
+//    }
     
     /// Calculates the straight line distance in meters between the current position and the next node.
-    func calculateStraightDistance() {
-        if let nextNode = self.nextNode, let frame = self.frame {
-            self.straightDistance = simd_distance(nextNode.simdTransform.columns.3, frame.camera.transform.columns.3) // Straight line distance in meters.
-        }
+    func calculateStraightDistance(cameraTransform: simd_float4x4, nextNodeTransform: simd_float4x4)->Float {
+       return simd_distance(nextNodeTransform.columns.3, cameraTransform.columns.3) // Straight line distance in meters.
     }
     
     /// Calculates the angle difference in degrees between the current direction and the direction to the next node.
-    func calculateAngleDifference() {
-        if let nextNode = self.nextNode, let frame = self.frame {
-            
-            // Direction we are currently facing.
-            let headingVec = simd_normalize(simd_float3(-frame.camera.transform.columns.2.x, 0.0, -frame.camera.transform.columns.2.z))
-            
-            // Direction we want to go.
-            let pointingVector = simd_float3(nextNode.simdTransform.columns.3.x - frame.camera.transform.columns.3.x,
-                                             0.0,
-                                             nextNode.simdTransform.columns.3.z - frame.camera.transform.columns.3.z)
-            
-            // Calculate angle between 2 vectors.
-            let q = simd_quaternion(headingVec, pointingVector)
-            self.angleDifference = q.angle * sign(q.axis.y)
-        }
-       
+    func calculateAngleDifference(cameraTransform: simd_float4x4, nextNodeTransform: simd_float4x4)->Float {
+        // Direction we are currently facing.
+        let headingVec = simd_normalize(simd_float3(-cameraTransform.columns.2.x, 0.0, -cameraTransform.columns.2.z))
+        
+        // Direction we want to go.
+        let pointingVector = simd_float3(nextNodeTransform.columns.3.x - nextNodeTransform.columns.3.x,
+                                         0.0,
+                                         nextNodeTransform.columns.3.z - nextNodeTransform.columns.3.z)
+        
+        // Calculate angle between 2 vectors.
+        let q = simd_quaternion(headingVec, pointingVector)
+        print("in angle diff: \(q.angle * sign(q.axis.y))")
+        return q.angle * sign(q.axis.y)
     }
     
     /// Updates the instance attributes with the given ARFrame and SCNNode, and calculates the straight line distance and the angle difference.
@@ -66,15 +57,10 @@ class PathFinderModel {
     /// - parameter nextNode: The next node in the path.
     ///
     /// - returns: The straight line distance and angle differene to the node.
-    func getDirections(frame: ARFrame, nextNode: SCNNode) -> (Float?, Float?){
-        // Update instance attributes.
-        self.frame = frame
-        self.nextNode = nextNode
-        
-        self.calculateStraightDistance()
-        self.calculateAngleDifference()
-        
-        return (self.straightDistance, self.angleDifference)
+    func getDirections(cameraTransform: simd_float4x4, nextNodeTransform: simd_float4x4) -> (Float?, Float?) {
+        let straightDistance = calculateStraightDistance(cameraTransform: cameraTransform, nextNodeTransform: nextNodeTransform)
+        let angleDiff = calculateAngleDifference(cameraTransform: cameraTransform, nextNodeTransform: nextNodeTransform)
+        return (straightDistance, angleDiff)
     }
     
 }
--- STEP NavigationTests/TestPathPlannerModel.swift
+++ STEP NavigationTests/TestPathPlannerModel.swift
@@ -2,34 +2,57 @@
 //  TestPathPlannerModel.swift
 //  STEP NavigationTests
 //
-//  Created by Olin Candidate on 4/10/23.
+//  Created by Sam Coleman on 4/10/23.
 //
 
+@testable import STEP_Navigation
 import XCTest
+import ARKit
+import ARCoreCloudAnchors
+import ARCoreGeospatial
+
+// Some simd_float4x4 extensions for making transforms more easily
+extension simd_float4x4 {
+    init(translation: simd_float3, yaw: Float) {
+        self.init(simd_quatf(angle: yaw, axis: simd_float3(0, 1, 0)))
+        self.columns.3 = simd_float4(translation, 1.0)
+    }
+}
 
 final class TestPathPlannerModel: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func setUpMock(angle: Float, distance: Float) -> (simd_float4x4, simd_float4x4) {
+        // we would expect back -pi/2 and then 10 meters
+//        let cameraTransform = simd_float4x4(translation: simd_float3(0, 0, 0), yaw: Float.pi/2)
+//        let nextNodeTransform = simd_float4x4(translation: simd_float3(10.0, 0, 0), yaw: 0.0)
+        
+        let cameraTransform = simd_float4x4(translation: simd_float3(0, 0, 0), yaw: angle)
+        let nextNodeTransform = simd_float4x4(translation: simd_float3(distance, 0, 0), yaw: 0.0)
+        
+        return (cameraTransform, nextNodeTransform)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testCalculateStraightDistance() throws {
+        let (cameraTranform, nextNodeTransform) = setUpMock(angle: Float.pi/2, distance: 10)
+        
+        let pathPlannerModel = PathPlannerModel()
+        let straightDistance = pathPlannerModel.calculateStraightDistance(cameraTransform: cameraTranform, nextNodeTransform: nextNodeTransform)
+        print("distance: \(straightDistance)")
+    }
+    
+    func testCalculateAngleDifference() throws {
+        let pathPlannerModel = PathPlannerModel()
+        let angleDifference = pathPlannerModel.calculateAngleDifference(cameraTransform: cameraTranform, nextNodeTransform: nextNodeTransform)
+        print("angle difference: \(angleDifference)")
     }
 
 }
