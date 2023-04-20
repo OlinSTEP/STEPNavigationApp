e474a4d1860b096434a9f0a0e734c3b68c3ea40c
Paul Ruvolo <paullundyruvolo@gmail.com>
2023-04-20 15:26:14 -0400

    Give an extra vote if the proposed pose matches the current pose.  Break ties based on most recently updated anchor.

--- STEP Navigation/Models/CloudAnchorAligner.swift
+++ STEP Navigation/Models/CloudAnchorAligner.swift
@@ -33,40 +33,46 @@ class CloudAnchorAligner {
     }
     
     func adjust(currentAlignment: simd_float4x4?)->simd_float4x4? {
-        var proposedAlignments: [String: simd_float4x4] = [:]
+        var proposedAlignments: [String: (Double, simd_float4x4)] = [:]
         for (cloudID, resolutions) in resolvedCloudAnchors {
             // TODO: maybe don't use last? if it jumped around too much?
             guard let lastResolution = resolutions.last, let mapTransform = cloudAnchorLandmarks?[cloudID] else {
                 continue
             }
             let anchorTransform = lastResolution.pose
-            proposedAlignments[cloudID] = anchorTransform.alignY() * mapTransform.alignY().inverse
+            proposedAlignments[cloudID] = (lastResolution.lastUpdateTime.timeIntervalSince1970, anchorTransform.alignY() * mapTransform.alignY().inverse)
         }
         // for right now we can filter based on
         guard let currentDevicePose = PositioningModel.shared.cameraTransform else {
             return currentAlignment
         }
         // TODO: fix bad O(n^2) algorithm
-        var votes: [(Int, simd_float4x4)] = []
+        var votes: [(Int, Double, simd_float4x4)] = []
         for (_, proposedAlignment) in proposedAlignments {
             var vote = 0
             var inlierPoses: [simd_float4x4] = []
-            inlierPoses.append(proposedAlignment)
-            let projectedPosition = proposedAlignment * currentDevicePose
-            for (_, proposedAlignment2) in proposedAlignments {
-                let projectedPosition2 = proposedAlignment2 * currentDevicePose
+            inlierPoses.append(proposedAlignment.1)
+            let projectedPosition = proposedAlignment.1 * currentDevicePose
+            var otherAlignments = proposedAlignments
+            // add the current alignment (if it exists) to provide a vote for the current alignment
+            if let currentAlignment = currentAlignment {
+                otherAlignments["current"] = (0.0, currentAlignment)
+            }
+            for (_, proposedAlignment2) in otherAlignments {
+                let projectedPosition2 = proposedAlignment2.1 * currentDevicePose
                 if simd_distance(projectedPosition.columns.3,
                                  projectedPosition2.columns.3) < 2.0 {
                     vote += 1
-                    inlierPoses.append(proposedAlignment2)
+                    inlierPoses.append(proposedAlignment2.1)
                 }
             }
-            
-            votes.append((vote, average(poses: inlierPoses)!))
+            votes.append((vote, proposedAlignment.0, proposedAlignment.1))
+
+            //votes.append((vote, proposedAlignment.0, average(poses: inlierPoses)!))
         }
         
-        // choose the one with the most votes.  If there are no votes, just stick with what we had originally
-        return votes.max(by: { $0.0 < $1.0 })?.1 ?? currentAlignment
+        // choose the one with the most votes.  If there is a tie, choose the most recent one. If there are no votes, just stick with what we had originally
+        return votes.max(by: { $0.0 < $1.0 || ($0.0 == $1.0 && $0.1 < $1.1 ) })?.2 ?? currentAlignment
     }
     
     private func average(poses: [simd_float4x4])->simd_float4x4? {
