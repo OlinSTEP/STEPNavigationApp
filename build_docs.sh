5ea81e420685701c516c8e20bd7cd561a9cf2b67
Paul Ruvolo <paullundyruvolo@gmail.com>
2023-05-10 12:33:25 -0400

    fixed build docs script

--- build_docs.sh
+++ build_docs.sh
@@ -5,4 +5,4 @@ xcodebuild docbuild -scheme "STEP Navigation" \
     -destination 'generic/platform=iOS';
 
 $(xcrun --find docc) process-archive transform-for-static-hosting \
-  "./docc/Build/Products/Debug-iphoneos/STEP Navigation.doccarchive" --output-path docs;
+  "./docc/Build/Products/Debug-iphoneos/STEP Navigation.doccarchive" --hosting-base-path STEPNavigationApp --output-path docs;
