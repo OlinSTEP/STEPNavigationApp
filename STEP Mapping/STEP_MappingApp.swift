56915dc287eb7aff9fa64e8006ac0bb3cc44e35f
Paul Ruvolo <paullundyruvolo@gmail.com>
2023-06-09 10:03:21 -0400

    Fixed a build error with step mapping

--- STEP Mapping/STEP_MappingApp.swift
+++ STEP Mapping/STEP_MappingApp.swift
@@ -24,3 +24,12 @@ struct STEP_MappingApp: App {
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+        
+    static var orientationLock = UIInterfaceOrientationMask.all //By default you want all your views to rotate freely
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
+}
